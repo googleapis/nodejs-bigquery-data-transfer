647fc56cff4de81d8b67e005b5fc76cbfe731e62
Jonathan Lui <jonathanlui@google.com>
2019-04-04 16:46:27 -0700

    Merge branch master

--- samples/system-test/quickstart.test.js
+++ samples/system-test/quickstart.test.js
@@ -15,9 +15,7 @@
 'use strict';
 
 const {assert} = require('chai');
-const cp = require('child_process');
-
-const execSync = (cmd) => cp.execSync(cmd, {encoding: 'utf-8'});
+const {execSync} = require('child_process');
 
 describe('quickstart', () => {
   it('should return a list of resources', async () => {
