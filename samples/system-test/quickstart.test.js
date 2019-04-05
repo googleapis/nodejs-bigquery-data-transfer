25f30290a7028f0ac263f5c2949cb69f1a7b6b0e
Jonathan Lui <jonathanlui@google.com>
2019-04-04 17:46:25 -0700

    fixit

--- samples/system-test/quickstart.test.js
+++ samples/system-test/quickstart.test.js
@@ -17,9 +17,7 @@
 const {assert} = require('chai');
 const cp = require('child_process');
 
-const execSync = (cmd) => cp.execSync(cmd, {encoding: 'utf-8'});
-
-const execSync = (cmd) => cp.execSync(cmd, {encoding: 'utf-8'});
+const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 
 describe('quickstart', () => {
   it('should return a list of resources', async () => {
