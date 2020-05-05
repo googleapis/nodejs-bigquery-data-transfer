ac281d417f92a9e5e8f1c223605cb0d7ef2ece3a
summerji <summerji@google.com>
2020-05-05 15:21:22 -0700

    fix: synth clean for src/index

--- src/index.ts
+++ src/index.ts
@@ -19,8 +19,6 @@
 import * as v1 from './v1';
 const DataTransferServiceClient = v1.DataTransferServiceClient;
 export {v1, DataTransferServiceClient};
-// For compatibility with JavaScript libraries we need to provide this default export:
-// tslint:disable-next-line no-default-export
 export default {v1, DataTransferServiceClient};
 import * as protos from '../protos/protos';
 export {protos};
